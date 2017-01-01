a39cecb508c395264a23e5ec2732ed5ef52a5184
Arie Blumenzweig <blumzi@013.net>
2017-01-01 19:47:09 +0200

    New Arduino code.

---
+++ FWArduino/Ascii.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class AsciiTable {
+public:
+	static const int STX = 2;
+	static const int ETX = 3;
+	static const int NL = 10;
+	static const int CR = 13;
+};
--- FWArduino/FWArduino.ino
+++ FWArduino/FWArduino.ino
@@ -10,11 +10,68 @@
 //      \    1   2   3   4   5   6   7   8   /
 //        \    9  10  11  12  13 14 15  /
 
+//
+// Glossary:
+//  The following terms are used:
+//
+//	position:
+//		- there are 8 positions on the filter wheel, distanced 200
+//			steps (full motor revolution) one from the other.
+//		- on the 8-filter wheel there are filters (and RFID tags) at every position, on
+//			the 4-filter wheel only one in two positions has a filter (and RFID tag).
+//		- at each position the optical detector gate-pin goes LOW
+//
+// tag:
+//	- RFID tags are located on the filter wheel, to identify the filter positions.
+//	- the RFID reader will attempt to read the tags ONLY when the optical slit is detected.
+//	- an RFID tag has 10 HEX characters, e.g. "7F0007F75E".
+//	- an empty RFID tag indicates the reader could not read a tag.  This can occur if:
+//		- there is no tag, e.g. in odd positions on a 4-filter wheel
+//		- the existing tag is faulty
+//
+// revolution:
+//		- 200 steps
+//
+//	speed:
+//		- the speed at which the stepper will rotate (STEPPER_NORMAL_SPEED)
+//
+//	direction:
+//		- the stepper direction: CW (positive number of steps) or CCW (negative number of steps)
+//
+//  Transport layer:
+//	  packet format:[STX][PAYLOAD][CRC 2][ETX]
+//		- STX - one byte, ASCII:2 (start-of-text)
+//		- ETX - one byte, ASCII:3 (end-of-text)
+//		- PAYLOAD - Even number of bytes (padded with NULL, if odd), ASCII printable characters ONLY
+//		- CRC - two bytes
+//
+//  Data layer:	command[:argument]
+//		- Commands may have optional arguments, separated by a colon (:)
+//
+//	Commands (always initiated by the PC):
+//
+//	1. Get position
+//		command: get-position, argument: none
+//		reply:   position:<tag> or position:no-tag
+//		operation:
+//			- If optical slot is NOT detected, search for nearest position (up to one motor revolution, first CW then CCW)
+//			- When optical slot is detected, read RFID tag.
+//			- If a <tag> was read, reply: position:<tag>
+//			- If no tag could be read, reply: position:no-tag
+//
+//	2. Move CW or CCW
+//		command: move:{CW|CCW}[n]
+//		reply:   position:<tag> or position:no-tag
+//		operation:
+//			- The arduino will rotate the motor n (default: 1) full revolution(s), in the specified direction
+//			- The arduino will perform exactly the same procedure as for the 'get-position' command
+//
 
 #include <Stepper.h>
 #include "Id12la.h"
-#define STEPPER_STEPS 1600
-#define NORMSPEED 37
+#define STEPPER_STEPS_PER_WHEEL8	1600
+#define STEPPER_STEPS_PER_REV		 200
+#define STEPPER_NORMAL_SPEED		  37
 // constants
 #if _ARDUINO_ == UNO
 #define STEPPER_PIN_1		47
@@ -32,10 +89,14 @@
 #define PHOTO_HIGH_PIN		34
 #define PHOTO_LOW_PIN		26
 #endif
-Stepper stepper(STEPPER_STEPS, STEPPER_PIN_1, STEPPER_PIN_2);
-const int largeStep = 100;// manual steps - large step
-const int mediumStep = 50; // manual steps - medium step
-const int smallStep = 5; // manual steps - small step
+
+#define CW(steps) (steps)		// clock-wise direction
+#define CCW(steps) (-(steps))	// counter-clock-wise direction
+
+Stepper stepper(STEPPER_STEPS_PER_REV, STEPPER_PIN_1, STEPPER_PIN_2);
+const int largeStep  = 100; // manual steps - large step
+const int mediumStep =  50; // manual steps - medium step
+const int smallStep  =   5; // manual steps - small step
 const int searchSteps = 30; // number of small step when looking for close detent
 
 							// dynamic vars
@@ -46,6 +107,7 @@ boolean smallMoves = false;
 boolean stillLooking;
 int direct;
 int i;
+char inBuf[256], outBuf[256];
 
 #if _ARDUINO_ == UNO
 Id12la tagReader(10, 11, 13);
@@ -53,8 +115,68 @@ Id12la tagReader(10, 11, 13);
 Id12la tagReader(13);	// TBD: RFID reset pin on MEGA
 #endif
 
+//
+// reads a packet from the PC, on the Serial port
+// blocks until a good packet is received (bad packets are silently discarded)
+//
+String readPacket() {
+	char *p;
+
+again:
+	// clean the inBuf
+	for (p = inBuf; (unsigned)(p - inBuf) < sizeof(inBuf); p++)
+		*p = 0;
+
+	// skip characters until we get a STX
+	Serial.find(AsciiTable::STX);
+
+	// store all characters up to ETX (or no more space in inBuf)
+	for (p = inBuf; (unsigned)(p - inBuf) < sizeof(inBuf); p++)
+		if ((*p = Serial.read()) == AsciiTable::ETX)
+			break;
+
+	if (*p != AsciiTable::ETX)
+		goto again;	// reached limit of inBuf without getting an ETX, start all over
+
+	// TODO: checksum
+
+	*p = 0;
+	return String(inBuf);
+}
+
+//
+// Sends a packet to the PC
+//
+void sendPacket(String payload) {
+	Serial.write(AsciiTable::STX);
+	Serial.print(payload);
+	Serial.write(AsciiTable::ETX);
+}
+
+//
+// Search for the optical slit.
+//  maxSteps:  max number of steps to move in each direction
+//  stepsPerMove: how much to move each time
+//
+bool searchForOpticalSlit(int maxSteps = STEPPER_STEPS_PER_REV, int stepsPerMove = 1) {
+	int count;
+
+	for (count = 1; count < maxSteps; count++) {
+		if (photoSlitDetected())
+			return true;
+		stepper.step(CW(stepsPerMove));
+	}
+	stepper.step(CCW(maxSteps));	// go back
+	for (count = 1; count < maxSteps; count++) {
+		if (photoSlitDetected())
+			return true;
+		stepper.step(CCW(stepsPerMove));
+	}
+	return false;
+}
+
 void setup() {
-	stepper.setSpeed(NORMSPEED);//define  the stepper speed
+	stepper.setSpeed(STEPPER_NORMAL_SPEED);//define  the stepper speed
 
 	Serial.begin(57600);	// opens serial port, sets data rate to 9600 bps
 	Serial.flush();
@@ -69,120 +191,56 @@ void setup() {
 	digitalWrite(STEPPER_COMMON_PIN, HIGH);
 
 	Serial.println("filter wheel ready ...");
+	
+	searchForOpticalSlit();
 }
 
-bool photoSlitInPosition() {
+//
+// The photo detector gate-pin drops to ground when
+//  it finds the positioning slit, and according to Ezra it
+//  is accurate, i.e. drops to ground if and only if it detected
+//  the slit.
+//
+bool photoSlitDetected() {
 	return digitalRead(PHOTO_GATE_PIN) == LOW;
 }
 
+String doGetPosition() {
+	String tag;
+
+	searchForOpticalSlit();
+	if ((tag = tagReader.readTag()) == NULL)
+		tag = String("no-tag");
+
+	return String("position:") + tag;
+}
+
+String doMove(int nPos) {
+	stepper.step(nPos * STEPPER_STEPS_PER_REV);
+	return doGetPosition();
+}
+
 void loop() {
-	int cmd = 0;// for incoming serial data
+	String command, reply;
 
-	terminate = 0;
-	PhotoInterEn = false;
-	moveSteps = 0;
-	smallMoves = false;
+	command = readPacket();
 
-	if (Serial.available() > 0) {
-
-		switch (cmd = Serial.read()) {
-		case 'T':
-			if (photoSlitInPosition()) {
-				char *tag = tagReader.readTag();
-				if (tag != NULL)
-					Serial.println(tag);
-			}
-			break;
-		case '1':
-			moveSteps = -1 * STEPPER_STEPS;
-			break;
-		case '2':
-			moveSteps = -2 * STEPPER_STEPS;
-			break;
-		case '3':
-			moveSteps = -3 * STEPPER_STEPS;
-			break;
-		case '4':
-			moveSteps = -4 * STEPPER_STEPS;
-			break;
-		case 'A':
-			moveSteps = STEPPER_STEPS;
-			break;
-		case 'B':
-			moveSteps = 2 * STEPPER_STEPS;
-			break;
-		case 'C':
-			moveSteps = 3 * STEPPER_STEPS;
-			break;
-		case 'a':
-			moveSteps = -largeStep;
-			smallMoves = true;
-			break;
-		case 'b':
-			moveSteps = largeStep;
-			smallMoves = true;
-			break;
-		case 'c':
-			moveSteps = -mediumStep;
-			smallMoves = true;
-			break;
-		case 'd':
-			moveSteps = mediumStep;
-			smallMoves = true;
-			break;
-		case 'e':
-			moveSteps = -smallStep;
-			smallMoves = true;
-			break;
-		case 'f':
-			moveSteps = smallStep;
-			smallMoves = true;
-			break;
-		case 'I':
-			// look for detent on close surrounding
-			stillLooking = true;
-			direct = 1;
-			i = 0;
-			while (stillLooking && i < searchSteps) {
-				stepper.step(direct*smallStep);
-				i++;
-				if (photoSlitInPosition())
-					stillLooking = false;
-			}
-
-			i = 0;
-			while (stillLooking && i < 2 * searchSteps) {
-				direct = -1;
-				stepper.step(direct*smallStep);
-				i++;
-				if (photoSlitInPosition())
-					stillLooking = false;
-			}
-			delay(500);
-			if (!photoSlitInPosition()) { // on some cases detent is reported on delay, so doing another check after the delay, and moving back 2 steps if needed
-				stepper.step(-2 * direct*smallStep);
-			}
-			break;
-		}
-
-		if (cmd == 'W') { // operating detent check and report
-			PhotoInterEn = true;
-			delay(100);
-		}
-		stepper.step(moveSteps);
-		if (moveSteps != 0 && smallMoves == false) {
-			Serial.print("P");
-			Serial.flush();
-			terminate = true;
-		}
-		if (PhotoInterEn == true) {
-			Serial.print(photoSlitInPosition() ? "D" : "d");
-			terminate = true;
-		}
-		if (terminate == true) {
-			Serial.print("R");
-			delay(500);
-			Serial.flush();
-		}
+	if (command.startsWith("get-position", 0)) {
+		reply = doGetPosition();
+	} else if (command.startsWith("moveCW:", 0)) {
+		int nPos = command.substring(strlen("moveCW:")).toInt();
+		if (nPos < 1 || nPos > 7)
+			reply = "bad-param";
+		else
+			reply = doMove(nPos);
 	}
+	else if (command.startsWith("moveCCW:", 0)) {
+		int nPos = command.substring(strlen("moveCCW:")).toInt();
+		if (nPos < 1 || nPos > 7)
+			reply = "bad-param";
+		else
+			reply = doMove(-nPos);
+	}
+	if (reply.length() > 0)
+		sendPacket(reply);
 }
