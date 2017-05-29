0d34ed4176be1bbe4076cf22ca82d489c9577052
mizpe-dome-ctlr <blumzi@013.net>
2017-05-29 17:25:47 +0300

    focuser version

--- Dash/AboutForm.cs
+++ Dash/AboutForm.cs
@@ -44,7 +44,7 @@ namespace Dash
             labelDashVersion.Text = appVersion;
             labelTelescopeVersion.Text = _dash.wisetele.DriverVersion;
             labelDomeVersion.Text = _dash.wisedome.DriverVersion;
-            labelFocuserVersion.Text = WiseFocuser.DriverVersion;
+            labelFocuserVersion.Text = WiseFocuser.Instance.DriverVersion;
             labelSafeToOpenVersion.Text = _dash.wisesite.safeToOpen.DriverVersion;
             labelBoltwoodVersion.Text = _dash.wiseboltwood.DriverVersion;
             labelComputerControlVersion.Text = _dash.wisesite.computerControl.DriverVersion;
