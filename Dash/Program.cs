fb9544670e6b8ac5b4c725d0c6b3e4e0d2953073
Arie Blumenzweig <blumzi@013.net>
2018-02-08 18:22:42 +0200

    Added --dio-monitor command-line-option

--- Dash/Program.cs
+++ Dash/Program.cs
@@ -14,26 +14,34 @@ namespace Dash
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // Set the unhandled exception mode to force all Windows Forms errors to go through
-            // our handler.
-            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
-            formDash = new FormDash();
+            if (args.Length == 1 && args[0] == "--dio-monitor")
+            {
+                Application.Run(new ASCOM.Wise40.HardwareForm());
+            }
+            else
+            {
 
-            Application.ApplicationExit += new EventHandler(formDash.OnApplicationExit);
+                // Set the unhandled exception mode to force all Windows Forms errors to go through
+                // our handler.
+                Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+                formDash = new FormDash();
 
-            // Add the event handler for handling UI thread exceptions to the event.
-            Application.ThreadException += new ThreadExceptionEventHandler(formDash.HandleThreadException);
+                Application.ApplicationExit += new EventHandler(formDash.OnApplicationExit);
 
-            // Add the event handler for handling non-UI thread exceptions to the event. 
-            AppDomain.CurrentDomain.UnhandledException +=
-                new UnhandledExceptionEventHandler(formDash.HandleDomainUnhandledException);
+                // Add the event handler for handling UI thread exceptions to the event.
+                Application.ThreadException += new ThreadExceptionEventHandler(formDash.HandleThreadException);
 
-            Application.Run(formDash);
+                // Add the event handler for handling non-UI thread exceptions to the event. 
+                AppDomain.CurrentDomain.UnhandledException +=
+                    new UnhandledExceptionEventHandler(formDash.HandleDomainUnhandledException);
+
+                Application.Run(formDash);
+            }
         }
     }
 }
