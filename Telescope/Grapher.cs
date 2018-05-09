1db20d2d74f494b8dde96ea51f67754e8d03b49a
Arie Blumenzweig <blumzi@013.net>
2018-05-09 19:52:31 +0300

    Added registration bat files

--- Telescope/Grapher.cs
+++ Telescope/Grapher.cs
@@ -15,9 +15,9 @@ namespace ASCOM.Wise40 //.Telescope
         public Grapher(TelescopeAxes axis,  double start, double target)
         {
             Angle.Type angleType = axis == TelescopeAxes.axisPrimary ? Angle.Type.RA : Angle.Type.Dec;
-            DateTime now = DateTime.Now;
+            DateTime utcNow = DateTime.UtcNow;
             string folder = Wise40.Common.Debugger.LogFolder() + 
-                string.Format("/slew-started-at-{0:D2}h{1:D2}m{2:D2}s", now.Hour, now.Minute, now.Second);
+                string.Format("/slew-started-at-{0:D2}h{1:D2}m{2:D2}s_UTC", utcNow.Hour, utcNow.Minute, utcNow.Second);
 
             Directory.CreateDirectory(folder);
             string filename = folder + string.Format("/{0}.dat",
@@ -28,7 +28,7 @@ namespace ASCOM.Wise40 //.Telescope
                 _sw = new StreamWriter(filename);
                 _sw.AutoFlush = true;
                 _sw.WriteLine("#");
-                _sw.WriteLine("# Started at: {0}", now.ToShortDateString());
+                _sw.WriteLine("# Started at: {0}", utcNow.ToShortDateString());
                 _sw.WriteLine("# Start position: {0}", start.ToString());
                 _sw.WriteLine("# Target position: {0}", target.ToString());
                 _sw.WriteLine("# Axis: {0}", axis.ToString());
---
+++ registerHomeDlls.bat
@@ -0,0 +1,26 @@
+set top=c:\Users\Blumzi\Documents\Visual Studio 2015\Projects\Wise40
+
+cd "%top%\Boltwood\bin\x86\Debug"
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm /codebase /tlb "%top%\Boltwood\bin\x86\Debug\ASCOM.Wise40.Boltwood.ObservingConditions.dll"
+
+cd "%top%\ComputerControl\bin\x86\Debug"
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm /codebase /tlb "%top%\ComputerControl\bin\x86\Debug\ASCOM.Wise40.ComputerControl.SafetyMonitor.dll"
+
+cd "%top%\Dome\bin\x86\Debug"
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm /codebase /tlb "%top%\Dome\bin\x86\Debug\ASCOM.Wise40.Dome.dll"
+
+cd "%top%\FilterWheel\bin\x86\Debug"
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm /codebase /tlb "%top%\FilterWheel\bin\x86\Debug\ASCOM.Wise40.FilterWheel.dll"
+
+cd "%top%\Focus\bin\x86\Debug"
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm /codebase /tlb "%top%\Focus\bin\x86\Debug\ASCOM.Wise40.Focuser.dll"
+
+cd "%top%\SafeToOpen\bin\x86\Debug"
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm /codebase /tlb "%top%\SafeToOpen\bin\x86\Debug\ASCOM.Wise40SafeToOpen.SafetyMonitor.dll"
+
+cd "%top%\Telescope\bin\x86\Debug"
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm /codebase /tlb "%top%\Telescope\bin\x86\Debug\ASCOM.Wise40.Telescope.dll"
+
+cd "%top%\VantagePro\bin\x86\Debug"
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm /codebase /tlb "%top%\VantagePro\bin\x86\Debug\ASCOM.Wise40.VantagePro.ObservingConditions.dll"
+
