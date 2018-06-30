548385aee4f7b5261f5d814f539f9d24ade296f1
Arie Blumenzweig <blumzi@013.net>
2018-06-30 13:42:54 +0300

    Added Wise40Watcher service

---
+++ Wise40Service/ProjectInstaller.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Wise40Watcher
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceInstallerWise40 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceInstallerWise40
+            // 
+            this.serviceInstallerWise40.Description = "Watches and restarts the Wise40 processes";
+            this.serviceInstallerWise40.DisplayName = "Wise40Watcher";
+            this.serviceInstallerWise40.ServiceName = "Wise40Watcher";
+            this.serviceInstallerWise40.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceInstallerWise40});
+
+        }
+
+        #endregion
+        private System.ServiceProcess.ServiceInstaller serviceInstallerWise40;
+    }
+}
---
+++ Wise40Service/Wise40Watcher.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using ASCOM.Wise40.Common;
+
+namespace Wise40Watcher
+{
+    public partial class Wise40Watcher : ServiceBase
+    {
+        WiseObject wiseobject = new WiseObject();
+        private Process serverProcess, dashProcess;
+        private bool _stopping = false;
+
+        private void serverExited(object sender, System.EventArgs e)
+        {
+            if (!_stopping)
+                StartServer();
+        }
+
+        private void dashExited(object sender, System.EventArgs e)
+        {
+            if (! _stopping)
+                StartDash();
+        }
+
+        public void StartServer()
+        {
+            serverProcess = new Process();
+            serverProcess.StartInfo.CreateNoWindow = true;
+            serverProcess.StartInfo.FileName = Const.wiseASCOMServerPath;
+            serverProcess.EnableRaisingEvents = true;
+            serverProcess.Exited += new EventHandler(serverExited);
+            serverProcess.Start();
+        }
+
+        public void StartDash()
+        {
+            dashProcess = new Process();
+            dashProcess.StartInfo.CreateNoWindow = true;
+            dashProcess.StartInfo.FileName = wiseobject.Simulated ? Const.wiseSimulatedDashPath : Const.wiseASCOMServerPath;
+            dashProcess.EnableRaisingEvents = true;
+            dashProcess.Exited += new EventHandler(dashExited);
+            dashProcess.Start();
+        }
+
+        public Wise40Watcher()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            StartServer();
+            StartDash();
+        }
+
+        protected override void OnStop()
+        {
+            _stopping = true;
+            serverProcess.Kill();
+            dashProcess.Kill();
+        }
+    }
+}
