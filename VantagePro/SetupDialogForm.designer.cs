cc34d51e3e78516f334a45161fc70a4063c8ca51
Arie Blumenzweig <blumzi@013.net>
2018-07-19 17:12:42 +0300

    Added serial port mode

--- VantagePro/SetupDialogForm.designer.cs
+++ VantagePro/SetupDialogForm.designer.cs
@@ -36,8 +36,16 @@ namespace ASCOM.Wise40.VantagePro
             this.labelReportFileValue = new System.Windows.Forms.Label();
             this.groupBoxReportFile = new System.Windows.Forms.GroupBox();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonDataFile = new System.Windows.Forms.RadioButton();
+            this.radioButtonSerialPort = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBoxSerialPort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picASCOM)).BeginInit();
             this.groupBoxReportFile.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // cmdOK
@@ -46,7 +54,7 @@ namespace ASCOM.Wise40.VantagePro
             this.cmdOK.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.cmdOK.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.cmdOK.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(161)))), ((int)(((byte)(142)))));
-            this.cmdOK.Location = new System.Drawing.Point(400, 76);
+            this.cmdOK.Location = new System.Drawing.Point(400, 193);
             this.cmdOK.Name = "cmdOK";
             this.cmdOK.Size = new System.Drawing.Size(59, 24);
             this.cmdOK.TabIndex = 0;
@@ -60,7 +68,7 @@ namespace ASCOM.Wise40.VantagePro
             this.cmdCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.cmdCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(161)))), ((int)(((byte)(142)))));
-            this.cmdCancel.Location = new System.Drawing.Point(400, 106);
+            this.cmdCancel.Location = new System.Drawing.Point(400, 223);
             this.cmdCancel.Name = "cmdCancel";
             this.cmdCancel.Size = new System.Drawing.Size(59, 25);
             this.cmdCancel.TabIndex = 1;
@@ -117,7 +125,7 @@ namespace ASCOM.Wise40.VantagePro
             this.groupBoxReportFile.Controls.Add(this.labelReportFileValue);
             this.groupBoxReportFile.Controls.Add(this.buttonChoose);
             this.groupBoxReportFile.ForeColor = System.Drawing.Color.DarkOrange;
-            this.groupBoxReportFile.Location = new System.Drawing.Point(8, 69);
+            this.groupBoxReportFile.Location = new System.Drawing.Point(32, 112);
             this.groupBoxReportFile.Name = "groupBoxReportFile";
             this.groupBoxReportFile.Size = new System.Drawing.Size(372, 62);
             this.groupBoxReportFile.TabIndex = 9;
@@ -129,12 +137,78 @@ namespace ASCOM.Wise40.VantagePro
             this.openFileDialog.Title = "WeatherLink Report File";
             this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonSerialPort);
+            this.groupBox1.Controls.Add(this.radioButtonDataFile);
+            this.groupBox1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.groupBox1.Location = new System.Drawing.Point(32, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(160, 40);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Operation mode ";
+            // 
+            // radioButtonDataFile
+            // 
+            this.radioButtonDataFile.AutoSize = true;
+            this.radioButtonDataFile.Checked = true;
+            this.radioButtonDataFile.Location = new System.Drawing.Point(16, 16);
+            this.radioButtonDataFile.Name = "radioButtonDataFile";
+            this.radioButtonDataFile.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonDataFile.TabIndex = 0;
+            this.radioButtonDataFile.TabStop = true;
+            this.radioButtonDataFile.Text = "Datafile";
+            this.radioButtonDataFile.UseVisualStyleBackColor = true;
+            this.radioButtonDataFile.Click += new System.EventHandler(this.radioButtonDataFile_Click);
+            // 
+            // radioButtonSerialPort
+            // 
+            this.radioButtonSerialPort.AutoSize = true;
+            this.radioButtonSerialPort.Location = new System.Drawing.Point(80, 16);
+            this.radioButtonSerialPort.Name = "radioButtonSerialPort";
+            this.radioButtonSerialPort.Size = new System.Drawing.Size(73, 17);
+            this.radioButtonSerialPort.TabIndex = 1;
+            this.radioButtonSerialPort.Text = "Serial Port";
+            this.radioButtonSerialPort.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.comboBoxSerialPort);
+            this.groupBox2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.groupBox2.Location = new System.Drawing.Point(32, 192);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(120, 64);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = " Serial Port ";
+            // 
+            // comboBoxSerialPort
+            // 
+            this.comboBoxSerialPort.FormattingEnabled = true;
+            this.comboBoxSerialPort.Location = new System.Drawing.Point(40, 28);
+            this.comboBoxSerialPort.Name = "comboBoxSerialPort";
+            this.comboBoxSerialPort.Size = new System.Drawing.Size(64, 21);
+            this.comboBoxSerialPort.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port:";
+            // 
             // SetupDialogForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(22)))), ((int)(((byte)(16)))));
-            this.ClientSize = new System.Drawing.Size(463, 173);
+            this.ClientSize = new System.Drawing.Size(463, 290);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBoxReportFile);
             this.Controls.Add(this.picASCOM);
             this.Controls.Add(this.label1);
@@ -151,6 +225,10 @@ namespace ASCOM.Wise40.VantagePro
             this.Load += new System.EventHandler(this.SetupDialogForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picASCOM)).EndInit();
             this.groupBoxReportFile.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -166,6 +244,12 @@ namespace ASCOM.Wise40.VantagePro
         private System.Windows.Forms.Label labelReportFileValue;
         private System.Windows.Forms.GroupBox groupBoxReportFile;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonSerialPort;
+        private System.Windows.Forms.RadioButton radioButtonDataFile;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxSerialPort;
         //private System.Windows.Forms.ComboBox comboBoxComPort;
     }
 }
