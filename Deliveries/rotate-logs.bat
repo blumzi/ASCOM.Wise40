081f345d92b9e8218d1ee3acc1fbe2c0f32ef17a
Arie Blumenzweig <blumzi@013.net>
2018-11-21 17:10:58 +0200

    Working

--- Deliveries/rotate-logs.bat
+++ Deliveries/rotate-logs.bat
@@ -1,2 +1,2 @@
-@echo off
-c:\cygwin64\bin\bash --login -c "cd /cygdrive/c/Wise40/Logs; ./rotate-logs"
+rem echo off
+c:\cygwin64\bin\bash --login -c "cd /cygdrive/c/Wise40/Logs; /usr/bin/bash -c /cygdrive/c/Wise40/Logs/rotate-logs"
