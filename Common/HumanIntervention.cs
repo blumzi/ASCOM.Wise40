210bb672b644fb4115a7461b757b005d33dd961e
Arie Blumenzweig <blumzi@013.net>
2018-11-29 14:28:40 +0200

    Split HumanIntervention and Moon from WiseSite

---
+++ Common/HumanIntervention.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+
+using ASCOM.Wise40.Common;
+
+namespace ASCOM.Wise40
+{
+
+    public static class HumanIntervention
+    {
+        static DateTime _lastInfoRead = DateTime.MinValue;
+        static string _info = null;
+
+        static HumanIntervention() { }
+
+        public static void Create(string oper, string reason)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(Const.humanInterventionFilePath));
+            using (StreamWriter sw = new StreamWriter(Const.humanInterventionFilePath))
+            {
+                sw.WriteLine("Operator: \"" + oper + "\"");
+                sw.WriteLine("Reason: \"" + reason + "\"");
+                sw.WriteLine("Created: " + DateTime.Now.ToString("MMM dd yyyy, hh:mm:ss tt"));
+            }
+
+            while (!File.Exists(Const.humanInterventionFilePath))
+            {
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+
+        public static void Remove()
+        {
+            if (!File.Exists(Const.humanInterventionFilePath))
+                return;
+
+            bool deleted = false;
+            while (!deleted)
+            {
+                try
+                {
+                    File.Delete(Const.humanInterventionFilePath);
+                    deleted = true;
+                }
+                catch (System.IO.IOException ex)    // in use
+                {
+                    ;
+                }
+            }
+        }
+
+        public static bool IsSet()
+        {
+            return System.IO.File.Exists(Const.humanInterventionFilePath);
+        }
+
+        public static string Info
+        {
+            get
+            {
+                string info = string.Empty;
+
+                if (!IsSet())
+                    return string.Empty;
+
+                if (File.GetLastWriteTime(Const.humanInterventionFilePath) > _lastInfoRead)
+                {
+
+                    StreamReader sr = new StreamReader(Const.humanInterventionFilePath);
+                    string line = string.Empty;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.StartsWith("Operator:") || line.StartsWith("Created:") || line.StartsWith("Reason:"))
+                            info += line + "; ";
+                    }
+
+                    info = "Human Intervention; " + ((info == string.Empty) ? string.Format("File \"{0}\" exists.",
+                        Const.humanInterventionFilePath) : info);
+                    _info = info.TrimEnd(';', ' '); ;
+                    _lastInfoRead = DateTime.Now;
+                }
+                return _info;
+            }
+        }
+    }
+}
---
+++ Common/Moon.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+using ASCOM.Astrometry.AstroUtils;
+using ASCOM.Astrometry.NOVAS;
+using ASCOM.Astrometry;
+using ASCOM.Wise40.Common;
+
+namespace ASCOM.Wise40
+{
+    public class Moon
+    {
+        private static AstroUtils astroutils = new AstroUtils();
+        private static NOVAS31 novas31 = new NOVAS31();
+        private static Object3 moon;
+        private static CatEntry3 dummy_star;
+        private static Observer observer;
+        private static Debugger debugger = Debugger.Instance;
+
+        // start Singleton
+        private static readonly Lazy<Moon> lazy =
+            new Lazy<Moon>(() => new Moon()); // Singleton
+
+        public static Moon Instance
+        {
+            get
+            {
+                lazy.Value.init();
+                return lazy.Value;
+            }
+        }
+
+        private Moon() { }
+        // end Singleton
+
+        private void init()
+        {
+            double[] ScPos = { 0, 0, 0 };
+            double[] ScVel = { 0, 0, 0 };
+            InSpace ObsSpace = new InSpace();
+
+            novas31.MakeInSpace(ScPos, ScVel, ref ObsSpace);
+            novas31.MakeObserver(ObserverLocation.EarthSurface, WiseSite.Instance.onSurface, ObsSpace, ref observer);
+            novas31.MakeCatEntry("DUMMY", "xxx", 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, ref dummy_star);
+            novas31.MakeObject(ObjectType.MajorPlanetSunOrMoon, 11, "Moon", dummy_star, ref moon);
+        }
+
+        public double Illumination
+        {
+            get
+            {
+                return astroutils.MoonIllumination(astroutils.JulianDateUT1(0));
+            }
+        }
+
+        public double Phase
+        {
+            get
+            {
+                return astroutils.MoonPhase(astroutils.JulianDateUT1(0));
+            }
+        }
+
+        //public short TopoPlanet(
+        //    double JdTt,
+        //    Object3 SsBody,
+        //    double DeltaT,
+        //    OnSurface Position,
+        //    Accuracy Accuracy,
+        //    ref double Ra,
+        //    ref double Dec,
+        //    ref double Dis
+        //)
+
+
+        ////8-----------------------------------------------------------------------------
+        //function SphereDist(Long1:extended; Lat1:extended; Long2:extended; Lat2:extended): extended;
+        ////------------------------------------------------------------------------------
+        //// calculate the spherical distance between two coordinates
+        //// Input : Long1 in radians [extended]
+        ////         Lat1  in radians [extended]
+        ////         Long2 in radians [extended]
+        ////         Lat2  in radians [extended]
+        //// Output: distance in radians [extended]
+        //// EO - May 2002
+        //        begin
+        //   Result := arccos(sin(Lat1)*sin(Lat2) + cos(Lat1)*cos(Lat2)*cos(Long1 - Long2));
+        //end;
+
+        public static double SphereDist(double long1, double lat1, double long2, double lat2)
+        {
+            return Math.Acos(Math.Sin(lat1) * Math.Sin(lat2) + Math.Cos(lat1) * Math.Cos(lat2) * Math.Cos(long1 - long2));
+        }
+
+        public Angle Distance(double telescopeRA, double telescopeDec)
+        {
+            SkyPos moonPos = new SkyPos();
+            short ret;
+
+            ret = novas31.Place(astroutils.JulianDateUT1(0),
+                moon,
+                observer,
+                0.0,
+                CoordSys.Astrometric,
+                Accuracy.Full,
+                ref moonPos);
+
+            if (ret != 0)
+            {
+                #region debug
+                debugger.WriteLine(Debugger.DebugLevel.DebugLogic, "Moon.Distance: Cannot calculate Moon position (ret: {0})", ret);
+                #endregion
+                return Angle.FromDegrees(0);
+            }
+
+            double rad = SphereDist(telescopeRA, telescopeDec, moonPos.RA, moonPos.Dec);
+            return Angle.FromRadians(rad, Angle.Type.Deg);
+        }
+    }
+}
