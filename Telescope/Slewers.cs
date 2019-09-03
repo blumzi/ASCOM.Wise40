654d7ccc402c51553dc135667efb1f0b42ff26ea
Arie Blumenzweig <blumzi@013.net>
2019-09-03 11:20:05 +0300

    Started using ConcurrentDictionary instead of List

--- Telescope/Slewers.cs
+++ Telescope/Slewers.cs
@@ -5,17 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 using ASCOM.Wise40.Common;
+using System.Collections.Concurrent;
 
 namespace ASCOM.Wise40
 {
     public class Slewers
     {
         private Debugger debugger = Debugger.Instance;
-        public enum Type { Dome, Ra, Dec, Focuser };
-        private Object _lock = new object();
+        public enum Type { Dome, Ra, Dec };
         private static volatile Slewers _instance;
         private static object syncObject = new object();
-        private static List<WiseTele.SlewerTask> _active = new List<WiseTele.SlewerTask>();
+        private static ConcurrentDictionary<Type, WiseTele.SlewerTask> _active = new ConcurrentDictionary<Type, WiseTele.SlewerTask>();
         private static ActivityMonitor activityMonitor = ActivityMonitor.Instance;
 
         public Slewers() { }
@@ -39,56 +39,46 @@ namespace ASCOM.Wise40
 
         public void Add(WiseTele.SlewerTask slewer)
         {
-            string before;
-
-            lock (_lock)
+            string before = _active.ToCSV();
+            if (!_active.TryAdd(slewer.type, slewer))
             {
-                before = ToString();
-                _active.Add(slewer);
+                #region debug
+                debugger.WriteLine(Common.Debugger.DebugLevel.DebugAxes, $"ActiveSlewers: {slewer} already active [{before}]");
+                #endregion
             }
+
             #region debug
-            debugger.WriteLine(Common.Debugger.DebugLevel.DebugAxes,
-                "ActiveSlewers: added {0}, \"{1}\" => \"{2}\"", slewer.ToString(), before, this.ToString());
+            debugger.WriteLine(Common.Debugger.DebugLevel.DebugAxes, $"ActiveSlewers: added {slewer}, [{before}] => [{_active.ToCSV()}]");
             #endregion
         }
 
         public bool Delete(Slewers.Type type)
         {
-            string before;
-
-            lock (_lock)
+            string before = _active.ToCSV();
+            if (!_active.TryRemove(type, out WiseTele.SlewerTask slewer))
             {
-                before = ToString();
+                #region debug
+                debugger.WriteLine(Debugger.DebugLevel.DebugLogic, $"ActiveSlewers: ignored removal of {type}, [{before}]");
+                #endregion
+                return false;
+            }
 
-                int index = _active.FindIndex((s) => s.type == type);
-                if (index == -1)
+            if (_active.Count == 0)
+            {
+                activityMonitor.EndActivity(ActivityMonitor.ActivityType.TelescopeSlew, new Activity.TelescopeSlew.EndParams()
                 {
-                    #region debug
-                    debugger.WriteLine(Debugger.DebugLevel.DebugLogic,
-                        "ActiveSlewers: Could not find a slewer of type {0} in _active", type.ToString());
-                    #endregion
-                    return false;
-                }
-                _active.RemoveAt(index);
-
-                if (_active.Count == 0)
-                {
-                    activityMonitor.EndActivity(ActivityMonitor.ActivityType.TelescopeSlew, new Activity.TelescopeSlew.EndParams()
-                        {
-                            endState = Activity.State.Succeeded,
-                            endReason = "Reached target",
-                            end = new Activity.TelescopeSlew.Coords
-                            {
-                                ra = WiseTele.Instance.RightAscension,
-                                dec = WiseTele.Instance.Declination,
-                            }
-                        });
-                }
+                    endState = Activity.State.Succeeded,
+                    endReason = "Reached target",
+                    end = new Activity.TelescopeSlew.Coords
+                    {
+                        ra = WiseTele.Instance.RightAscension,
+                        dec = WiseTele.Instance.Declination,
+                    }
+                });
             }
 
             #region debug
-            debugger.WriteLine(Common.Debugger.DebugLevel.DebugAxes,
-                "ActiveSlewers: deleted {0}, \"{1}\" => \"{2}\" ({3})", type.ToString(), before, this.ToString(), _active.GetHashCode());
+            debugger.WriteLine(Debugger.DebugLevel.DebugLogic, $"ActiveSlewers: removed {type}, [{before}] => [{_active.ToCSV()}]");
             #endregion
             return true;
         }
@@ -97,71 +87,33 @@ namespace ASCOM.Wise40
         {
             get
             {
+                int count = _active.Count;
+
                 #region debug
-                debugger.WriteLine(Common.Debugger.DebugLevel.DebugAxes,
-                    "ActiveSlewers: Count {0} \"{1}\"", _active.Count, this.ToString());
+                debugger.WriteLine(Common.Debugger.DebugLevel.DebugAxes, $"ActiveSlewers: Count {count} [{ToString()}]");
                 #endregion
-                return _active.Count;
+                return count;
             }
         }
 
         public override string ToString()
         {
-            List<string> s = new List<string>();
-            lock (_lock)
-            {
-                foreach (var a in _active)
-                    s.Add(a.type.ToString());
-            }
-
-            return string.Join(",", s.ToArray());
+            return _active.ToCSV();
         }
 
         public bool Active(WiseTele.SlewerTask slewer)
         {
-            bool ret = false;
-
-            lock (_lock)
-                foreach (var a in _active)
-                {
-                    if (a.type == slewer.type)
-                    {
-                        ret = true;
-                        break;
-                    }
-                }
-            return ret;
+            return _active.ContainsKey(slewer.type);
         }
 
         public bool Active(Slewers.Type type)
         {
-            bool ret = false;
-
-            lock (_lock)
-                foreach (var a in _active)
-                {
-                    if (a.type == type)
-                    {
-                        ret = true;
-                        break;
-                    }
-                }
-            return ret;
+            return _active.ContainsKey(type);
         }
 
         public void Clear()
         {
-            lock(_lock)
-                _active.Clear();
-        }
-
-        public Task[] ToArray()
-        {
-            List<Task> tasks = new List<Task>();
-
-            foreach (var a in _active)
-                tasks.Add(a.task);
-            return tasks.ToArray();
+            _active.Clear();
         }
     }
 }
