3066fa81ed87a0683d061e3ddc3923a62a60c9ba
Arie Blumenzweig <blumzi@013.net>
2020-08-24 09:38:57 +0300

    Added global suppresion for catch

---
+++ Telescope/GlobalSuppressions.cs
@@ -0,0 +1,26 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.AxisMonitor.StartMovementChecker")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.DomeSlaveDriver.FindHome")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.DomeSlaveDriver.Init")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.DomeSlaveDriver.Park")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.DomeSlaveDriver.SlewToAz(System.Double,System.String)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.Pulsing.Start(ASCOM.DeviceInterface.GuideDirections,System.Int32)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.SlewPlotter.#ctor(ASCOM.DeviceInterface.TelescopeAxes,System.Double,System.Double)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.TelescopeSetupDialogForm.BrowseToAscom(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.WiseTele.AbortSlew(System.String)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.WiseTele.DoShutdown(System.String)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.WiseTele.DoSlewToCoordinatesAsync(ASCOM.Wise40.Common.Angle,ASCOM.Wise40.Common.Angle,System.String)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.WiseTele.GenericDomeSlewerTask(System.Action)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.WiseTele.Init")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.WiseTele.InternalMoveAxis(ASCOM.DeviceInterface.TelescopeAxes,System.Double,ASCOM.Wise40.Common.Const.AxisDirection,System.Boolean)~System.Boolean")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.WiseTele.MoveToKnownHaDec(ASCOM.Wise40.Common.Angle,ASCOM.Wise40.Common.Angle)~System.String")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.WiseTele.PulseGuide(ASCOM.DeviceInterface.GuideDirections,System.Int32)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.WiseTele.SlewToCoordinates(System.Double,System.Double,System.Boolean)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:ASCOM.Wise40.WiseTele.SlewToCoordinatesAsync(System.Double,System.Double,System.Boolean)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~P:ASCOM.Wise40.WiseTele.Status")]
