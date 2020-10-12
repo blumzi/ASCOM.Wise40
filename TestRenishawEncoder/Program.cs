99049596bd4bd6b4e94c4f8d26cdbb63951eb8c2
Arie Blumenzweig <blumzi@013.net>
2020-10-12 08:40:14 +0300

    Encoders are 32 bit

--- TestRenishawEncoder/Program.cs
+++ TestRenishawEncoder/Program.cs
@@ -21,7 +21,7 @@ namespace TestRenishawEncoder
 
             int ret;
             const int nEncoders = 1;
-            const int nBits = 26;
+            const int nBits = 32 + 1 + 1 + 6;   // 32 data, 1 error, 1 warn, 6 crc
 
             byte[] channels = { 0 };
             byte[] dataLengths = { nBits };
@@ -41,7 +41,7 @@ namespace TestRenishawEncoder
             }
 
             ret = Board.BissMasterInitSingleCycle(moduleNbr: moduleNumber,
-                sensorDataFreqDivisor: 0,
+                sensorDataFreqDivisor: 17,
                 registerDataFreqDivisor: 0,
                 channel0BISSSSIMode: (byte)ChannelMode.BISS,
                 channel0BISSMode: (byte)BissMode.C,
@@ -58,11 +58,16 @@ namespace TestRenishawEncoder
             if (ret != 0)
                 Environment.Exit(1);
 
-            ret = Board.BissMasterSingleCycleDataRead(moduleNbr: moduleNumber, slaveIndex: 0, out uint value, out _);
+            ret = Board.BissMasterSingleCycleDataRead(moduleNbr: moduleNumber, slaveIndex: 0, out UInt32 low, out UInt32 high);
+            UInt32 crc = (uint)(low & ~(1 << 6));
+            bool warn = (low & (1 << 7)) != 0;
+            bool error = (low & (1 << 8)) != 0;
+            UInt64 value = (((high << 32) | low) >> 8) & 0xffffffff;
+
             Debug.WriteLine($"BissMasterSingleCycleDataRead: ret == {ret}");
             if (ret != 0)
                 Environment.Exit(2);
-            Debug.WriteLine($"value: {value}");
+            Debug.WriteLine($"value: {value}, error: {error}, warn: {warn}, crc: {crc}");
 
             ret = Board.BissMasterReleaseSingleCycle(moduleNbr: moduleNumber);
             Debug.WriteLine($"BissMasterReleaseSingleCycle: ret == {ret}");
