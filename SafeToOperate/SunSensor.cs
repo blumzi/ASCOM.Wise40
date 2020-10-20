14ae0365d10ea20ce5a74009f4111f512c7654a7
Arie Blumenzweig <blumzi@013.net>
2020-10-20 08:44:03 +0300

    Use of IpGeolocation API instead of novas31 to get Sun elevation

--- SafeToOperate/SunSensor.cs
+++ SafeToOperate/SunSensor.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using ASCOM.Wise40;
 using ASCOM.Wise40.Common;
+using System.Threading;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+using System.Net;
 
 namespace ASCOM.Wise40SafeToOperate
 {
@@ -20,14 +25,18 @@ namespace ASCOM.Wise40SafeToOperate
         private bool _wasSafe = false;
         private string _status;
 
-        public SunSensor(WiseSafeToOperate instance) :
+        public SunSensor(WiseSafeToOperate wiseSafeToOperate) :
             base("Sun",
                 Attribute.SingleReading |
                 Attribute.Periodic |
                 Attribute.AlwaysEnabled |
-                Attribute.CanBeBypassed,
+                Attribute.CanBeBypassed |
+                Attribute.CanBeStale,
                 "°", " deg", "f1", "",
-                instance) { }
+                wiseSafeToOperate)
+        {
+            Init();
+        }
 
         public override object Digest()
         {
@@ -60,18 +69,27 @@ namespace ASCOM.Wise40SafeToOperate
                 return null;
 
             double max = DateTime.Now.Hour < 12 ? _maxAtDawn : _maxAtDusk;
+            TimeSpan ts = DateTime.Now.Subtract(_lastDataRead);
+            bool stale = ts > _maxTimeBetweenIpGeolocationReads;
+            double elevation = SunElevation;
 
             Reading r = new Reading
             {
-                Stale = false,
-                Usable = true,
-                Safe = wisesafetooperate.SunElevation <= max,
-                value = wisesafetooperate.SunElevation,
-                timeOfLastUpdate = DateTime.Now,
-                secondsSinceLastUpdate = 0,
+                Stale = stale,
+                Usable = !Double.IsNaN(_elevation) && !stale,
+                Safe = !Double.IsNaN(elevation) && elevation <= max,
+                value = elevation,
+                timeOfLastUpdate = _lastDataRead,
+                secondsSinceLastUpdate = ts.TotalSeconds,
             };
 
-            _status = $"Sun elevation is {FormatVerbal(wisesafetooperate.SunElevation)} (max: {FormatVerbal(max)})";
+            if (Double.IsNaN(elevation))
+                _status = "Sun elevation is not available yet";
+            else if (r.Stale)
+                _status = $"Sun elevation is stale (older than {_maxTimeBetweenIpGeolocationReads.ToMinimalString()})";
+            else
+                _status = $"Sun elevation is {FormatVerbal(SunElevation)} (max: {FormatVerbal(max)})";
+
             if (r.Safe != _wasSafe)
             {
                 activityMonitor.Event(new Event.SafetyEvent(
@@ -86,8 +104,13 @@ namespace ASCOM.Wise40SafeToOperate
 
         public override string UnsafeReason()
         {
-            double currentElevation = wisesafetooperate.SunElevation;
+            double currentElevation = SunElevation;
             double max = DateTime.Now.Hour < 12 ? _maxAtDawn : _maxAtDusk;
+
+            if (Double.IsNaN(currentElevation))
+                return "Sun elevation is not available yet";
+            else if (DateTime.Now.Subtract(_lastDataRead) > _maxTimeBetweenIpGeolocationReads)
+                return $"Sun elevation is stale (older than {_maxTimeBetweenIpGeolocationReads.ToMinimalString()})";
 
             return currentElevation <= max ? "" : $"The Sun elevation ({FormatVerbal(currentElevation)}) is higher than {FormatVerbal(max)}";
         }
@@ -127,11 +150,181 @@ namespace ASCOM.Wise40SafeToOperate
         }
 
         public override string MaxAsString { get { return ""; } set { } }
+
+        public double SunElevation
+        {
+            get
+            {
+                return _elevation;
+            }
+        }
+
+        private void PeriodicReader(object state)
+        {
+            if (Interlocked.CompareExchange(ref _readingInterlock, 1, 0) == 0)
+                return;
+            GetIpGeolocationInfo().GetAwaiter().GetResult();
+            Interlocked.Exchange(ref _readingInterlock, 0);
+        }
+
+        public async Task GetIpGeolocationInfo()
+        {
+            int maxTries = 10, tryNo;
+
+            DateTime start = DateTime.Now;
+            TimeSpan duration = TimeSpan.Zero;
+
+            HttpResponseMessage response = null;
+            for (tryNo = 0; tryNo < maxTries; tryNo++)
+            {
+                try
+                {
+                    response = await _client.GetAsync(URL).ConfigureAwait(false);
+                    duration = DateTime.Now.Subtract(start);
+                    break;
+                }
+                catch (HttpRequestException ex)
+                {
+                    duration = DateTime.Now.Subtract(start);
+                    #region debug
+                    debugger.WriteLine(Debugger.DebugLevel.DebugSafety,
+                        $"GetIpGeolocationInfo: try#: {tryNo}, HttpRequestException: {ex.Message} at {ex.StackTrace}, duration: {duration}");
+                    #endregion
+                    continue;
+                }
+                catch (TaskCanceledException)
+                {
+                    duration = DateTime.Now.Subtract(start);
+                    #region debug
+                    debugger.WriteLine(Debugger.DebugLevel.DebugSafety,
+                        $"GetIpGeolocationInfo: try#: {tryNo}, timedout, duration: {duration}");
+                    #endregion
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    duration = DateTime.Now.Subtract(start);
+                    #region debug
+                    debugger.WriteLine(Debugger.DebugLevel.DebugSafety,
+                        $"GetIpGeolocationInfo: try#: {tryNo}, Exception: {ex.Message} at {ex.StackTrace}, duration: {duration}");
+                    #endregion
+                    continue;
+                }
+            }
+
+            if (response != null)
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    IpGeolocationInfo info;
+
+                    try
+                    {
+                        info = JsonConvert.DeserializeObject<IpGeolocationInfo>(content);
+                        _elevation = info.sun_altitude;
+                        _lastDataRead = DateTime.Now;
+                        #region debug
+                        debugger.WriteLine(Debugger.DebugLevel.DebugSafety,
+                            "GetIpGeolocationInfo: success: got " +
+                            $"elevation: {Angle.FromDegrees(_elevation, Angle.AngleType.Alt).ToShortNiceString()} " +
+                            $"after {tryNo + 1} tries " +
+                            $"in {duration.ToMinimalString()}");
+                        #endregion
+                    }
+                    catch (Exception ex)
+                    {
+                        #region debug
+                        debugger.WriteLine(Debugger.DebugLevel.DebugSafety,
+                            $"GetIpGeolocationInfo: caught {ex.Message} at\n{ex.StackTrace}");
+                        #endregion
+                        return;
+                    }
+                    return;
+                }
+                else
+                {
+                    #region debug
+                    debugger.WriteLine(Debugger.DebugLevel.DebugSafety,
+                        $"GetIpGeolocationInfo: try#: {tryNo}, HTTP failure: StatusCode: {response.StatusCode}, ReasonPhrase: {response.ReasonPhrase} duration: {duration}");
+                    #endregion
+                }
+            }
+            else
+            {
+                #region debug
+                debugger.WriteLine(Debugger.DebugLevel.DebugSafety,
+                    $"GetIpGeolocationInfo: try#: {tryNo}, HTTP response == null, duration: {duration}");
+                #endregion
+            }
+        }
+
+        private const string apiKey = "d6ce0c7ecb5c451ba2b462dfb5750364";
+
+        private static readonly string URL = "https://api.ipgeolocation.io/astronomy?" +
+            $"apiKey={apiKey}&" +
+            $"lat={WiseSite.Instance.Latitude.Degrees}&" +
+            $"long={WiseSite.Instance.Longitude.Degrees}";
+
+        private DateTime _lastDataRead = DateTime.MinValue;
+        private TimeSpan _timeBetweenIpGeolocationReads = TimeSpan.FromMinutes(2);
+        private TimeSpan _maxTimeBetweenIpGeolocationReads = TimeSpan.FromMinutes(5);
+        public HttpClient _client;
+        public Timer _periodicReadTimer;
+        private bool _initialized = false;
+        private int _readingInterlock = 0;
+        private static double _elevation = Double.NaN;
+
+        public void Init()
+        {
+            if (_initialized)
+                return;
+
+            WiseName = "SunSensor";
+
+            _client = new HttpClient();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/html"));
+            _client.Timeout = TimeSpan.FromSeconds(10);
+
+            _periodicReadTimer = new Timer(new TimerCallback(PeriodicReader));
+            _periodicReadTimer.Change(0, (int) _timeBetweenIpGeolocationReads.TotalMilliseconds);
+
+            _initialized = true;
+        }
     }
 
     public class SunDigest
     {
         public string Name;
         public bool IsSafe;
+    }
+
+    public class IpGeolocationLocation
+    {
+        public double latitude;
+        public double longitude;
+    }
+
+    public class IpGeolocationInfo
+    {
+        public IpGeolocationLocation location;
+        public DateTime date;
+        public DateTime current_time;
+        public DateTime sunrise;
+        public DateTime sunset;
+        public string sun_status;
+        public DateTime solar_noon;
+        public TimeSpan day_length;
+        public double sun_altitude;
+        public double sun_distance;
+        public double sun_azimuth;
+        public DateTime moonrise;
+        public DateTime moonset;
+        public string moon_status;
+        public double moon_altitude;
+        public double moon_distance;
+        public double moon_azimuth;
+        public double moon_parallactic_angle;
     }
 }
