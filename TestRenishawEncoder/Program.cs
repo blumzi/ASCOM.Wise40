058db7a36af8f197698d7705af596b8f29034a4f
Arie Blumenzweig <blumzi@013.net>
2020-10-27 09:47:34 +0200

    Seems to work but not sure about the values.

--- TestRenishawEncoder/Program.cs
+++ TestRenishawEncoder/Program.cs
@@ -4,77 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using PCIe1711_NET;
-using System.Diagnostics;
-
 namespace TestRenishawEncoder
 {
     public static class Program
     {
-        private enum ChannelMode { BISS = 0, SSI = 1 };
-        private const byte CRCPolynom = ((1 << 6) | (1 << 1) | (1 << 0));
-        private enum BissMode { B = 0, C = 1 };
-
-        public static void Main()
+        public static void Main(string[] argv)
         {
-            PCIe1711 Board = null;
+            Hardware.RenishawEncoder encoder = null;
+            int encNumber = 1;
 
-            int ret;
-            const int nEncoders = 1;
-            const int nBits = 32 + 1 + 1 + 6;   // 32 data, 1 error, 1 warn, 6 crc
-
-            byte[] channels = { 0 };
-            byte[] dataLengths = { nBits };
-            byte[] options = { 0 };
-            byte[] polynoms = { CRCPolynom };
-            byte[] inverts = { 1 };
-            const byte moduleNumber = 1;
-            const int boardNo = 0;
+            if (argv.Length != 0)
+                encNumber = Convert.ToInt32(argv[0]);
 
             try
             {
-                Board = PCIe1711.OpenBoard(boardNo);
-            } catch (Exception ex)
+                encoder = new Hardware.RenishawEncoder(encNumber);
+            }
+            catch (Exception ex)
             {
-                Debug.WriteLine($"OpenBoard caught {ex.Message}");
+                Console.WriteLine($"Failed to create RenishawEncoder({encNumber}): caught {ex.Message}");
                 Environment.Exit(1);
             }
 
-            ret = Board.BissMasterInitSingleCycle(moduleNbr: moduleNumber,
-                sensorDataFreqDivisor: 17,
-                registerDataFreqDivisor: 0,
-                channel0BISSSSIMode: (byte)ChannelMode.BISS,
-                channel0BISSMode: (byte)BissMode.C,
-                channel1BISSSSIMode: 0,
-                channel1BISSMode: 0,
-                nbrOfSlave: nEncoders,
-                channel: channels,
-                dataLength: dataLengths,
-                option: options,
-                CRCPolynom: polynoms,
-                CRCInvert: inverts);
-
-            Debug.WriteLine($"BissMasterInitSingleCycle: ret == {ret}");
-            if (ret != 0)
-                Environment.Exit(1);
-
-            ret = Board.BissMasterSingleCycleDataRead(moduleNbr: moduleNumber, slaveIndex: 0, out UInt32 low, out UInt32 high);
-            UInt32 crc = (uint)(low & ~(1 << 6));
-            bool warn = (low & (1 << 7)) != 0;
-            bool error = (low & (1 << 8)) != 0;
-            UInt64 value = (((high << 32) | low) >> 8) & 0xffffffff;
-
-            Debug.WriteLine($"BissMasterSingleCycleDataRead: ret == {ret}");
-            if (ret != 0)
-                Environment.Exit(2);
-            Debug.WriteLine($"value: {value}, error: {error}, warn: {warn}, crc: {crc}");
-
-            ret = Board.BissMasterReleaseSingleCycle(moduleNbr: moduleNumber);
-            Debug.WriteLine($"BissMasterReleaseSingleCycle: ret == {ret}");
-            if (ret != 0)
-                Environment.Exit(3);
-
-            Debug.WriteLine("Done.");
+            for (; ; )
+            {
+                try
+                {
+                    Console.WriteLine($"{encoder.Position}");
+                    System.Threading.Thread.Sleep(500);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to get encoder position: Exception: {ex.Message}");
+                    Environment.Exit(2);
+                }
+            }
         }
     }
 }
---
+++ TestRenishawEncoder/RenishawEncoder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PCIe1711_NET;
+
+namespace Hardware
+{
+    /// <summary>
+    /// <para>
+    /// Hardware:
+    ///  - one ADDI APCIe-1711 card
+    ///  - two Renishaw Resolute encoders, model RL32BBT001B99A (BISS 32 bit)
+    /// Functional modules 0 and 1 on the APCIe-1711 are configured for BISS protocol
+    /// </para>
+    /// <para>
+    /// One encoder is slave #0 of the functional module #0.
+    /// The other encoder is slave #0 of the functional module #1.
+    /// </para>
+    /// </summary>
+    public class RenishawEncoder
+    {
+        private enum ChannelMode { BISS = 0, SSI = 1 };
+        private const byte CRCPolynom = ((1 << 6) | (1 << 1) | (1 << 0));
+        private enum BissMode { B = 0, C = 1 };
+        private static readonly PCIe1711 Board = PCIe1711.OpenBoard(0);
+        private readonly byte _moduleNumber;
+        public RenishawEncoder(int encoderNumber)
+        {
+            int ret;
+
+            byte[] channels = { 0 };
+            byte[] options = { 0 };
+            byte[] polynoms = { CRCPolynom };
+            byte[] inverts = { 1 };
+            byte[] dataLengths = { 35 };
+            _moduleNumber = (byte)encoderNumber;
+
+            ret = Board.BissMasterInitSingleCycle(moduleNbr: _moduleNumber,
+                sensorDataFreqDivisor: 17,
+                registerDataFreqDivisor: 0,
+                channel0BISSSSIMode: (byte)ChannelMode.BISS,
+                channel0BISSMode: (byte)BissMode.B,
+                channel1BISSSSIMode: 0,
+                channel1BISSMode: 0,
+                nbrOfSlave: 1,
+                channel: channels,
+                dataLength: dataLengths,
+                option: options,
+                CRCPolynom: polynoms,
+                CRCInvert: inverts);
+            System.Threading.Thread.Sleep(500);
+
+            if (ret != 0)
+                throw new Exception($"BissMasterInitSingleCycle(moduleNumber: {_moduleNumber}) returned {ret}");
+        }
+        public UInt32 Position
+        {
+            get
+            {
+                string op = $"Renishaw({_moduleNumber}).Position";
+                int ret = Board.BissMasterSingleCycleDataRead(
+                    moduleNbr: _moduleNumber,
+                    slaveIndex: 0,
+                    out UInt32 low,
+                    out UInt32 high);
+
+                if (ret != 0)
+                    throw new Exception($"{op}: BissMasterSingleCycleDataRead returned {ret}");
+
+                UInt64 reading = (high << 32) | low;
+                bool warning = (reading & (1 << 0)) == 0;
+                bool error = (reading & (1 << 1)) == 0;
+                UInt32 position = (UInt32) ((reading >> 2) & 0xffffffff);
+
+                if (warning)
+                    throw new Exception($"{op}: encoder or scale need cleaning");
+                if (error)
+                    throw new Exception($"{op}: encoder read error");
+
+                return position;
+            }
+        }
+        ~RenishawEncoder()
+        {
+            Board.BissMasterReleaseSingleCycle(moduleNbr: _moduleNumber);
+        }
+    }
+}
