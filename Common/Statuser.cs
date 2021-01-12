dfa5b93e27cfab8f1853526b34df4196d4c61efe
Arie Blumenzweig <blumzi@013.net>
2021-01-12 08:04:41 +0200

    Some cleanup

--- Common/Statuser.cs
+++ Common/Statuser.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
 
-
 namespace ASCOM.Wise40.Common
 {
     public class Statuser
@@ -19,31 +18,23 @@ namespace ASCOM.Wise40.Common
                 { Severity.Error, Color.IndianRed },
                 { Severity.Good, Color.Green },
             };
-
-        private Label _label;
-        private ToolTip _toolTip;
+        private readonly ToolTip _toolTip;
         private DateTime _expiration;
-        private System.Windows.Forms.Timer _timer;
+        private readonly Timer _timer;
         private bool _busy = false;
 
         public Statuser(Label label, ToolTip tooltip = null)
         {
-            this._label = label;
-            _timer = new Timer();
-            _timer.Interval = 100;
+            this.Label = label;
+            _timer = new Timer
+            {
+                Interval = 100
+            };
             _timer.Tick += Tick;
             _toolTip = tooltip;
         }
 
-        public Label Label
-        {
-            get
-            {
-                return _label;
-            }
-        }
-
-
+        public Label Label { get; }
         public void Show(string s, int millis = 0, Severity severity = Severity.Normal, bool silent = false)
         {
             if (_busy)
@@ -55,10 +46,10 @@ namespace ASCOM.Wise40.Common
             if (s == null)
                 return;
 
-            if (_label != null)
+            if (Label != null)
             {
-                _label.ForeColor = colors[severity];
-                _label.Text = s;
+                Label.ForeColor = colors[severity];
+                Label.Text = s;
             }
 
             if (millis > 0)
@@ -68,10 +59,9 @@ namespace ASCOM.Wise40.Common
                 _busy = true;
             }
         }
-
         public void SetToolTip(string tip)
         {
-            _toolTip.SetToolTip(_label, tip);
+            _toolTip.SetToolTip(Label, tip);
         }
 
         private void Tick(object sender, EventArgs e)
@@ -80,9 +70,9 @@ namespace ASCOM.Wise40.Common
 
             if (now.CompareTo(_expiration) > 0)
             {
-                _label.Text = "";
-                if (_toolTip != null)
-                    _toolTip.SetToolTip(_label, "");
+                Label.Text = "";
+                _toolTip?.SetToolTip(Label, "");
+
                 _expiration = now;
                 _timer.Stop();
                 _busy = false;
